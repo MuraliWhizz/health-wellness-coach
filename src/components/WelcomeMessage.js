62a61da6b2c89437269c9d36184ba206c57f3119
MuraliWhizz <157956338+MuraliWhizz@users.noreply.github.com>
2024-08-27 21:53:55 +0530

    WelcomeMessage.js

--- src/components/WelcomeMessage.js
+++ src/components/WelcomeMessage.js
@@ -3,7 +3,7 @@ import React from 'react';
 function WelcomeMessage() {
   return (
     <div className="welcome-message">
-      <p>Your personal health journey begins here. The chat will open automatically to start your assessment.</p>
+      <p>Your personal health journey begins here. The chat will open automatically to start your assistance.</p>
       <div className="pulse-icon">
         <span>&#128172;</span>
       </div>
